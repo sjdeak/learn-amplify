fafe256084b6bec5a2b778a8452feb4936f79806
Jun Shen <sjdeak@yahoo.com>
2023-12-23 16:12:18 +0800

    chore: add wait

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,33 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Habit } from "./models/index.js";
 import { DataStore } from "aws-amplify/datastore";
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Hub } from "aws-amplify/utils";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    const unsubscribeHubListen = Hub.listen("datastore", async (hubData) => {
+      const {event, data} = hubData.payload;
+      console.log(`Event: ${JSON.stringify(event)} Data: ${JSON.stringify(data)}`);
+    });
+
+    return () => {
+      unsubscribeHubListen();
+    };
+  }, []);
 
   const handleClick = async () => {
     const newHabit = await DataStore.save(new Habit({name: "V1", count: 1})); // Step 1
     const habitJustSaved = await DataStore.query(Habit, newHabit.id); // Step 2
     console.log({habitJustSaved});
 
+    console.log("Start to wait 5 seconds");
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    console.log("Start to save modified habit");
     const modified = Habit.copyOf(habitJustSaved, updated => { // Step 3
       updated.name = "V2";
     });
